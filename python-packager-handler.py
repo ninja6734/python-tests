31e330e630fb0edf6c093c61d65e9d095a9f40ae
ninja6734 <fortnite.skurka@web.de>
2024-03-12 20:21:13 +0100

    commit

--- python-packager-handler.py
+++ python-packager-handler.py
@@ -1,4 +1,5 @@
 import scratchattach as sa
+from io import BytesIO
 from PIL import Image
 import requests
 from scratchattach import Encoding
@@ -9,8 +10,18 @@ CloudEvents = sa.CloudEvents(project_id = 683289707)
 
 def generatePfp(user):
     userVar = sa.get_user(user)
-    req = requests.get(userVar.icon_url)
-    print(userVar.icon_url)
+    req = requests.get(userVar.icon_url).content
+    req = BytesIO(req)
+    Img = Image.open(req)
+    pixelList = []
+    for pixelX in Img.width:
+        for pixelY in Img.height:
+            pixelList.append(Img.getpixel((pixelX, pixelY)))
+    return pixelList
+
+def sendPackage(package):
+    connection.set_var("testing", package)
+
 
 @CloudEvents.event
 def on_set(event):
