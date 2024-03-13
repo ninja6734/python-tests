333655b225e3d830d2343bb384040d2bcb554792
ninja6734 <fortnite.skurka@web.de>
2024-03-13 14:44:43 +0100

    commit

--- python-packager-handler.py
+++ python-packager-handler.py
@@ -12,32 +12,49 @@ CloudEvents = sa.CloudEvents(project_id = 683289707)
 
 MaxNumberLimit = 256
 
+def rgb_to_hex(rgb):
+    r, g, b = rgb
+    r = max(0, min(r, 255))
+    g = max(0, min(g, 255))
+    b = max(0, min(b, 255))
+    
+    hex_color = "{:02X}{:02X}{:02X}".format(r, g, b)
+    
+    return hex_color
+
 def generatePfp(user):
     userVar = sa.get_user(user)
     req = requests.get(userVar.icon_url).content
     req = BytesIO(req)
     Img = Image.open(req)
+    Img = Img.convert("RGB")
+    Img.resize((15,15))
     pixelList = []
-    for pixelX in Img.width:
-        for pixelY in Img.height:
-            pixelList.append(Img.getpixel((pixelX, pixelY)))
+    for pixelX in range(Img.width):
+        for pixelY in range(Img.height):
+            pixel = Img.getpixel((pixelX, pixelY))
+            pixel = rgb_to_hex(pixel)
+            pixelList.append(pixel)
     return pixelList
 
 def splitToPackages(package):
-    if (len(package) <= MaxNumberLimit - 7):
-        return [str(random.randint(1,9999999)) + str(package)]
+    if (len(package) <= MaxNumberLimit - 8):
+        return [str(random.randint(1,99999999)).zfill(8) + str(package)]
     else:
         packages = []
-        for i in range(0,len(package),MaxNumberLimit - 7):
-            packages.append(str(random.randint(1,9999999))+str(package)[i:i+MaxNumberLimit-7])
+        for i in range(0,len(package),MaxNumberLimit - 8):
+            packages.append(str(random.randint(1,99999999)).zfill(8)+str(package)[i:i+MaxNumberLimit-8])
         return packages
 
 def sendPackage(package):
-    packages = splitToPackages(package)
+    packages = splitToPackages(Encoding.encode(package))
     for i in packages:
         connection.set_var("testing",i)
+        print("sent package: "+ Encoding.decode(i[8:]))
         #be nice :)
-        time.sleep(0.5)
+        time.sleep(0.2)
+    connection.set_var("testing",str(random.randint(1,99999999)).zfill(8)+str(Encoding.encode("end")))
+    print("sent package ending")
 
 
 @CloudEvents.event
@@ -45,5 +62,13 @@ def on_set(event):
     out = Encoding.decode(event.value)
     action = out.split(";")
     if action[0] == "pfp":
-        send = generatePfp(action[1])
+        send = ""
+        for color in generatePfp(action[1]):
+            send += color
+        sendPackage(send)
+
+@CloudEvents.event
+def on_ready():
+    print("listener ready!")
+
 CloudEvents.start()
--- python-testing.py
+++ python-testing.py
@@ -1,13 +1,15 @@
 import random
+from scratchattach import Encoding
 MaxNumberLimit = 256
 
 def splitToPackages(package):
     if (len(package) <= MaxNumberLimit - 7):
-        return [str(random.randint(1,9999999)) + str(package)]
+        return [str(random.randint(1,9999999)).zfill(7) + str(package)]
     else:
         packages = []
         for i in range(0,len(package),MaxNumberLimit - 7):
-            packages.append(str(random.randint(1,9999999))+str(package)[i:i+MaxNumberLimit-7])
+            packages.append(str(random.randint(1,9999999)).zfill(7)+str(package)[i:i+MaxNumberLimit-7])
         return packages
 
-print(splitToPackages(("banana")*50))
+test = "bananas are really amazing and wonderful and observationary and I like them really much and I'm running out of ideas what to write here"
+print(splitToPackages(Encoding.encode(test)))
